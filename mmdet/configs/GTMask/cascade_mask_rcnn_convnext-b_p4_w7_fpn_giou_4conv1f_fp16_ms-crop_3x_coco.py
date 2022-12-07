20b96234eed7627baa295ff63f5aff5c2736cdb0
Shi Yi Lan <shiyil@nvidia.com>
2022-12-07 00:10:17 -0800

    Copy configs and training scripts from mmdet URL: https://github.com/open-mmlab/mmdetection Files:
    https://github.com/open-mmlab/mmdetection/tree/5fb38fa4fc4a822ba6ced3b8c2e3dcefa6efacec/configs/_base_
    -> mmdet/configs/_base_
    
    https://github.com/open-mmlab/mmdetection/blob/5fb38fa4fc4a822ba6ced3b8c2e3dcefa6efacec/configs/mask2former/*.py
    -> mmdet/configs/GTMask
       mmdet/configs/MALMask
    
    https://github.com/open-mmlab/mmdetection/tree/5fb38fa4fc4a822ba6ced3b8c2e3dcefa6efacec/configs/convnext/*.py
    -> mmdet/configs/GTMask
       mmdet/configs/MALMask
    
    https://github.com/open-mmlab/mmdetection/tree/5fb38fa4fc4a822ba6ced3b8c2e3dcefa6efacec/configs/solov2/*.py
    -> mmdet/configs/GTMask
       mmdet/configs/MALMask
    
    https://github.com/open-mmlab/mmdetection/tree/5fb38fa4fc4a822ba6ced3b8c2e3dcefa6efacec/tools
    -> mmdet/tools
    
    Date obtained: 12/07/2022
    Version: 5fb38fa4fc4a822ba6ced3b8c2e3dcefa6efacec
    Version SHA: https://github.com/open-mmlab/mmdetection/tree/5fb38fa4fc4a822ba6ced3b8c2e3dcefa6efacec

---
+++ mmdet/configs/GTMask/cascade_mask_rcnn_convnext-b_p4_w7_fpn_giou_4conv1f_fp16_ms-crop_3x_coco.py
@@ -0,0 +1,37 @@
+_base_ = './cascade_mask_rcnn_convnext-t_p4_w7_fpn_giou_4conv1f_fp16_ms-crop_3x_coco.py'  # noqa
+
+# please install mmcls>=0.22.0
+# import mmcls.models to trigger register_module in mmcls
+custom_imports = dict(imports=['mmcls.models'], allow_failed_imports=False)
+checkpoint_file = 'https://download.openmmlab.com/mmclassification/v0/convnext/convnext-base_3rdparty_32xb128-noema_in1k_20220222-dba4f95f.pth'  # noqa
+
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='mmcls.ConvNeXt',
+        arch='base',
+        out_indices=[0, 1, 2, 3],
+        drop_path_rate=0.6,
+        layer_scale_init_value=1.0,
+        gap_before_final_norm=False,
+        init_cfg=dict(
+            type='Pretrained', checkpoint=checkpoint_file,
+            prefix='backbone.')),
+    neck=dict(in_channels=[128, 256, 512, 1024]))
+
+optimizer = dict(
+    _delete_=True,
+    constructor='LearningRateDecayOptimizerConstructor',
+    type='AdamW',
+    lr=0.0002,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg={
+        'decay_rate': 0.7,
+        'decay_type': 'layer_wise',
+        'num_layers': 12
+    })
+
+data = dict(test=dict(
+            ann_file='data/coco/annotations/image_info_test-dev2017.json',
+            img_prefix='data/coco/test2017/'))
